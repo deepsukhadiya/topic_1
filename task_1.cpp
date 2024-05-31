ad1cc4e407205b5dc2cc58df05757280768c6d56
Deep Sukhadiya <deepsukhadiya36@gmail.com>
2024-05-31 12:27:01 +0530

    Add files via upload

---
+++ task_1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+main()
+{
+	printf("Name: deep sukhadiya\n");
+	printf("Age : 16\n");
+	printf("school: J.B & karp impex vidya sankul\n");
+	printf("hobby: runing\n");
+}
---
+++ task_2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	printf("---------\n");
+	printf("|\t|\n");
+	printf("D\t|\n");
+	printf("E\t|\n");
+	printf("E\t|\n");
+	printf("P\t|\n");
+	printf("|\t|\n");
+	printf("|\t|\n");
+	printf("---------\n");
+}
